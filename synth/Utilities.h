0a9323d3353477549948402ad2500e191375a2ed
Matthew Walker <matthew.walker9@gmail.com>
2018-10-08 20:30:51 +0100

    Changed mod matrix #defines to const std::string

--- synth/Utilities.h
+++ synth/Utilities.h
@@ -13,18 +13,16 @@ namespace chordial
 {
 namespace synth
 {
-
 // MOD NAMES
 // SOURCES
-#define GLOBAL_LFO1_OUT "lfo1_out"
-#define VOICE_ADSR1_OUT "adsr1_out"
-#define VOICE_ADSR2_OUT "adsr2_out"
+    const std::string GLOBAL_LFO1_OUT("lfo1_out");
+    const std::string VOICE_ADSR1_OUT("adsr1_out");
+    const std::string VOICE_ADSR2_OUT("adsr2_out");
 
 // DESTINATIONS
-#define GLOBAL_OSC_MASTER_FM_IN "Master_Osc_FM_input"
-#define GLOBAL_FILTER_MASTER_CUTOFF_IN "Master_Filter_Cutoff_In"
-#define VOICE_FILTER_MASTER_CUTOFF_IN "Voice_Filter_Cutoff_In"
-#define VOICE_DCA_GAIN_IN "dca_in"
-
+    const std::string GLOBAL_OSC_MASTER_FM_IN("Master_Osc_FM_input");
+    const std::string GLOBAL_FILTER_MASTER_CUTOFF_IN("Master_Filter_Cutoff_In");
+    const std::string VOICE_FILTER_MASTER_CUTOFF_IN("Voice_Filter_Cutoff_In");
+    const std::string VOICE_DCA_GAIN_IN("dca_in");
 }
 }
